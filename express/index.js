9a9dc9fd89bd09d6eea2fe7e8b05a6c1c927e65a
Agrashant7 <prashantag420@gmail.com>
2023-03-03 00:09:14 +0530

    index changes

--- express/index.js
+++ express/index.js
@@ -16,14 +16,17 @@ const corsOptions ={
   credentials:true,            //access-control-allow-credentials:true
   optionSuccessStatus:200,
 }
+router.get('/', function(req, res){
+  res.writeHead(200, { 'Content-Type': 'text/html' });
+  res.write('<h1>Hello from Express.js!</h1>');
+  res.end();
+});
 router.get('/test', function(req, res){
   res.writeHead(200, { 'Content-Type': 'text/html' });
   res.write('<h1>Hello from Express.js!</h1>');
   res.end();
 });
  
-app.use(express.static('public'));
-app.use(cors(corsOptions));
 app.use('/.netlify/functions/index', router);
 
   router.post('/upload-single-file', function(req, res){
